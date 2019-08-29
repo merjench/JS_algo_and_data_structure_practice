a9b5b226812a08b6cc3db35b336f8c745abf5e5f
merjench <merjen.chary@gmail.com>
2019-08-29 17:28:09 -0400

    Big-O-Notation: sum of numbers

---
+++ Big_O_Notation/REAMME.md
@@ -0,0 +1,11 @@
+
+# If not time, then what?
+   Rather than counting seconds, which are so variable...
+     Let's count the number of simple operatios the computer has to perform!
+
+# Big O Definition:
+  - We say that an algorithms is O(f(n)) if the number of simple operations the computer has to do is eventually less than a constant times f(n), as n increases.
+  * f(n) could be linear (f(n) = n) // as in a function the n grows so does the operations
+  * f(n) could be quadratic (f(n) =n2)
+  * f(n) could be constant (f(n) = 1) //even if n changes the operations don't change
+  * f(n) could be something entirely different
--- Big_O_Notation/sum_of_numbers.js
+++ Big_O_Notation/sum_of_numbers.js
@@ -1,6 +1,8 @@
 // sum of all numbers from 1 upt to whatever n is
 //all this code is on my chrome snippets
 
+//operations: there is +, =, ++ (additions and assignments), 0, i = 1; i <= n
+//over all as n grows the operations also grows on the solution below
 function addUpTo(n) {
     let total = 0;
     for (let i=1; i <= n; i++) {
@@ -11,6 +13,7 @@ function addUpTo(n) {
 console.log(addUpTo(100))
 
 // another solution
+// there are 3 operations/calculations happening: *, +, /
 function addUpTo(n) {
     return n* (n+1)/2;
 }
@@ -19,8 +22,9 @@ console.log(addUpTo(100))
 
 //which solution is better?
 //What does better mean?  Fastes? Faster memory-intensity? More readable?
-// since faster and memor-intensity is usually the most importan, why not use timers?
+// since faster and memory-intensity is usually the most important, why not use timers?
 
+//hence the second operation is faster
 
 let t1 = performance.now();
 addUpTo(1000000000)
